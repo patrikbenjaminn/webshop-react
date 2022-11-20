5925856f105a60c02be6aa06fc72c56c27babe9e
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-11-20 19:48:20 +0200

    korjattu contact css

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-/* import React from 'react'
+import React from 'react'
 import '../styles/Contact.css';
 import { useState } from 'react';
 
@@ -41,4 +41,4 @@ import { useState } from 'react';
         </>
     );
 }
-export default Contact; */
+export default Contact;
