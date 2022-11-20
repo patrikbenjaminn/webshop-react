366e63cbf06d557bcb7cd743c294b9e5137c0a3b
markov76 <101133846+markov76@users.noreply.github.com>
2022-11-20 16:36:38 +0200

    contact sivu lisätty

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Etusivu from './pages/Etusivu';
 import { Routes, Route } from 'react-router-dom'
+import NotFound from './pages/NotFound';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
 
        <Routes>
           <Route path='/' element={ <Etusivu />} />
+          <Route path='/Contact' element={ <Contact />} />
+
+          <Route path='*' element={ <NotFound />} />
+
        </Routes>
 
 
