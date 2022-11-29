a27bedfdaf356ec605c62b1e84cc859d06002f50
markov76 <101133846+markov76@users.noreply.github.com>
2022-11-29 21:10:21 +0200

    ad

--- src/pages/Tuoteryhmät.js
+++ src/pages/Tuoteryhmät.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-export default function Tuoteryhmät() {
+export default function Tuoteryhmät({url, valittuKategoria, setValittuKategoria}) {
+  const [kategoria, setKategoria] = useState([]);
+
+  useEffect(() => {
+    axios.get(url + 'tuoteryhma')
+  })
+
   return (
     <div>Tuoteryhmät</div>
   )
