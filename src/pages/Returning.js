4e6fea35db73563665a8e0b48b30853071051b9c
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-12-02 10:46:49 +0200

    aloiteltu search, palautusohjeet lisätty

--- src/pages/Returning.js
+++ src/pages/Returning.js
@@ -5,16 +5,19 @@ import '../styles/Contact.css';
 function Return() {
     return (
         <>
-       
+
             <div className="contact-form">
-            <div className="contact-form__header">
-                    <h2>Palautusohjeet - Tämä on kesken</h2>
-                    <p5>-Tarjoamme asiakkaillemme 30 päivän palautusoikeuden.</p5>
-                   <p5>- Huolehdi tuotteesta ja sen mukana toimitettavista tarvikkeista, ohjeista ja tuotepakkauksesta, kunnes olet päättänyt pitää tuotteen.<br></br>
-                    - Palautuksen syytä ei tarvitse sen kummemmin eritellä, vaan ostoksen voi palauttaa niin halutessaan.<br></br>
-                    - Palautukset ja hyvitykset käsitellään noin kahden viikon sisällä tuotteen lähetyksestä. Hyvityksestä ilmoitetaan sähköpostitse.
-                    </p5>
-            </div>
+                <div className="contact-form__header">
+                    <h2>Palautusohjeet</h2>
+                    Tarjoamme asiakkaillemme 30 päivän palautusoikeuden. Huolehdithan tuotteesta
+                    ja sen mukana toimitettavista tarvikkeista, ohjeista ja tuotepakkauksesta,
+                    kunnes olet päättänyt pitää tuotteen. Palautuksen syytä ei tarvitse eritellä,
+                    vaan ostoksen voi palauttaa niin halutessaan.
+                    Voit palauttaa tuotteen veloituksetta sekä Postin että Matkahuollon kautta.
+                    Palautettavan tuotteen mukaan tulee liittää palautuslomake, jossa tiedot palautettavasta tuotteesta.
+                    Pakkaa tuote käyttöohjeineen ja tarvikkeineen huolellisesti alkuperäispakettiin tai vastaavaan,
+                     kuljetusta kestävään laatikkoon ja vie lähimpään Postin tai Matkahuollon palautuspisteeseen.
+                </div>
             </div>
 
         </>
