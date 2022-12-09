2ad37fe43d4ca9db05bcf07f87a2ea707674e1e5
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-12-09 18:51:15 +0200

    lisätty search.css

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import '../styles/Contact.css';
+import '../styles/Search.css';
 import '../App.css';
 import { Navigate } from 'react-router-dom';
+
 
 
 const url = 'http://localhost/webshop/php/';
@@ -11,17 +12,16 @@ const url = 'http://localhost/webshop/php/';
 
 export default function Searchproducts() {
 
-
     const [name, setName] = useState('');
     const [products, setProducts] = useState([]);
-    const [search, setSearch] = useState("");
+    const [search, setSearch] = useState([]);
 
 
     function executeSearch(e) {
         if (e.charCode === 13) {
             e.preventDefault();
-            Navigate('/search/' + search);
-            alert("you have searched for - " + search);
+            Navigate('/Search/' + search);
+       
         }
     }
 
@@ -58,26 +58,26 @@ export default function Searchproducts() {
 
     return (
         <>
-            <form className="form-inline my-2 my-lg-0">
+            <form className="form-inline">
                 <input
                     value={search}
       
                     onChange={(e)=>setSearch(e.target.value)}
                     onKeyDown={(e)=>executeSearch(e)}
-                    className="form-control mr-sm-2"
+                    className="form-control"
                     type="search"
                     placeholder="Etsi tuotteita"
                     aria-label='Search'
                 />           
-                <div>
+             
+            </form>
+            <div>
                     <h3> {name} </h3>
                     {products?.map(tuote => (
                         <li key={tuote.tuoteid}>{tuote.tuotenimi}{tuote.hinta}</li>
                     ))
                     }
                 </div>
-            </form>
-
         </>
     );
 }
