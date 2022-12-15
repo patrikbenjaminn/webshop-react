3b9ab5ff6e4ed964cdd9632b1c150ffc0eb4d62f
markov76 <101133846+markov76@users.noreply.github.com>
2022-12-15 19:16:12 +0200

    admin etenee

---
+++ src/pages/admin/ListProduct.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListProduct() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListProduct
---
+++ src/pages/admin/ShowProducts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShowProducts() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShowProducts
