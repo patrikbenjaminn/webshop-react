992966ab62c93b36376d2e8a231b4adcb00722f8
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-12-16 02:34:26 +0200

    .

--- src/pages/Tarjoussivu.js
+++ src/pages/Tarjoussivu.js
@@ -1,49 +1,79 @@
 import React from 'react'
 import '../styles/Tarjoussivu.css'
 import '../products/Products'
-function Tarjoussivu() {
+import { useState, useEffect } from 'react'
+import '../styles/Tuotteet.css'
+
+function handleClick(addToCart){
+  return
+}
+const Tarjoussivu = () => {
+  const [tarjoukset, setTarjoukset] = useState([])
+
+  const fetchData = () => {
+    fetch("http://localhost/webshop/php/admin/tarjoussivu.php")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setTarjoukset(data)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    
-    <section className="tarjoukset" style={{ textalign: "center"}}>
+
+    <section className="tarjoukset" style={{ textalign: "center" }}>
       <div className="container py-5">
         <h1 className="text-center">Päivän tarjoukset</h1>
         <div className="row py-5">
-          <div className="col-lg-3">
-            <div className="card h-100">
-              <img src="../images/Video Game Console.H03.2k.png" className="img-fluid mb-3" alt=""/>
-              <input type="button" value="OSTA TÄSTÄ"/>   
-              <h5>Tuotenimi</h5>
-              <p className=''><small><del>80€</del><span>60€</span></small></p>
+          {tarjoukset.map(tarjous => (
+            <div className="col">
+              <div className="card h-100">
+                <img src={"../../images/" + tarjous.img} className="img-fluid mb-3" alt="" />
+                <input type="button" onClick={handleClick} value="OSTA TÄSTÄ" />
+                <h3 className="tuotenimi">{tarjous.tuotenimi}  </h3>
+                <p><span className='tuote'> {tarjous.tarjoushinta}</span></p>
+              </div>
             </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="card h-100">
-              <img src="../images/Snail Ring Game.H03.2k.png" className="img-fluid mb-3" alt=""/>
-              <input type="button" value="OSTA TÄSTÄ"/>
-              <h5>Tuotenimi</h5>
-              <p className=''><small><del>80€</del><span>60€</span></small></p>
+          ))}
+        {/*   {tarjoukset.map(tuote => (
+            <div className="col-lg-3">
+              <div className="card h-100">
+                <img src={"../../images/" + tuote.img} className="img-fluid mb-3" alt="" />
+                <input type="button" onClick={handleClick} value="OSTA TÄSTÄ" />
+                <h3 className="tuotenimi">{tuote.tuotenimi}  </h3>
+                <p><span className='tuote'> {tuote.hinta}</span></p>
+              </div>
             </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="card h-100">
-              <img src="../images/Hockey Table.H03.2k.png" className="img-fluid mb-3" alt=""/>
-              <input type="button" value="OSTA TÄSTÄ"/>
-              <h5>Tuotenimi</h5>
-              <p className=''><small><del>80€</del><span>60€</span></small></p>
+          ))} */}
+     {/*      {tarjoukset.map(tuote => (
+            <div className="col-lg-3">
+              <div className="card h-100">
+                <img src={"../../images/" + tuote.img} className="img-fluid mb-3" alt="" />
+                <input type="button" onClick={handleClick} value="OSTA TÄSTÄ" />
+                <h3 className="tuotenimi">{tuote.tuotenimi}  </h3>
+                <p><span className='tuote'> {tuote.hinta}</span></p>
+              </div>
             </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="card h-100">
-              <img src="../images/old-video-game-2022-10-14-18-25-41-utc-removebg-preview.png" className="img-fluid mb-3" alt=""/>
-              <input type="button" value="OSTA TÄSTÄ"/>
-              <h5>Tuotenimi</h5>
-              <p className=''><small><del>80€</del><span>60€</span></small></p>
+          ))}
+          {tarjoukset.map(tuote => (
+            <div className="col-lg-3">
+              <div className="card h-100">
+                <img src={"../../images/" + tuote.img} className="img-fluid mb-3" alt="" />
+                <input type="button" onClick={handleClick} value="OSTA TÄSTÄ" />
+                <h3 className="tuotenimi">{tuote.tuotenimi}  </h3>
+                <p><span className='tuote'> {tuote.hinta}</span></p>
+              </div>
             </div>
-          </div>
+          ))} */}
         </div>
       </div>
     </section>
-        
+
   )
 }
 
