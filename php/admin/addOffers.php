887554bfcac450a078ab52f595262b0f18f91878
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-12-18 18:47:57 +0200

    tarjous korjattu

--- php/admin/addOffers.php
+++ php/admin/addOffers.php
@@ -4,24 +4,21 @@ require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
 $input = json_decode(file_get_contents('php://input'));
-$tuotenimi = filter_var($input->tuotenimi,FILTER_SANITIZE_SPECIAL_CHARS);
-$normihinta = filter_var($input->normihinta,FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
-$tarjoushinta= filter_var($input->tarjoushinta,FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);;
-$img = filter_var($input->img,FILTER_SANITIZE_SPECIAL_CHARS);
+$tuoteid = filter_var($input->tuoteid,FILTER_SANITIZE_NUMBER_FLOAT);
+$tarjoushinta = filter_var($input->tarjoushinta,FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
+
 
 try {
   $db=openDb();
-  $query = $db->prepare('insert into tarjous(tuotenimi,normihinta,tarjoushinta,img)
-  values (:tuotenimi,:normihinta,:tarjoushinta,:img)');
-  $query->bindValue(':tuotenimi', $tuotenimi,PDO::PARAM_STR);
-  $query->bindValue(':normihinta', $normihinta,PDO::PARAM_STR);
+  $query = $db->prepare('update tuote set tarjoushinta=:tarjoushinta where tuoteid=:tuoteid');
+  $query->bindValue(':tuoteid', $tuoteid,PDO::PARAM_STR);
   $query->bindValue(':tarjoushinta', $tarjoushinta,PDO::PARAM_STR);
-  $query->bindValue(':img', $img,PDO::PARAM_STR);
   $query->execute();
   header('HTTP/1.1 200 OK');
-  $data = array('id' => $db->lastInsertId(),'tuotenimi' => $tuotenimi,'normihinta' => $normihinta,
-  'tarjoushinta' => $tarjoushinta,'img' => $img);
+  $data = array('tuoteid' =>$tuoteid,'tarjoushinta' => $tarjoushinta);
   print json_encode($data);
 } catch (PDOException $pdoex) {
   returnError($pdoex);
   }
+
+  ?>
